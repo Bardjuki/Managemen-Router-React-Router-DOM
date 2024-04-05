5ba37ab217301dcdeaff70cfa08853c60b5fe40a
Bardjuki <ahmadbardjukis@gmail.com>
2024-04-05 21:36:23 +0700

    update-deploy

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -6,7 +6,7 @@ import Footer from '../../Components/Footer'
 const HomePage = () => {
   return (
     <div>
-        <NavbarComponent/>
+    <NavbarComponent/>
     <div>HomePage</div>
     <div>satu</div>
     <div>dua</div>
